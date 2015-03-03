9bccf569131f0a73000804c4d9993910403a4636
Akshat Vasavada <akshatvas@gmail.com>
2015-03-03 14:02:12 -0800

    added alternate goals page

--- routes/goals.js
+++ routes/goals.js
@@ -1,5 +1,11 @@
 var goals = require('../goals.json');
 
 exports.view = function(req, res){
-	res.render('goals', goals);
+	var random_num = Math.random();
+
+  if(random_num > 0.5){
+    res.render("goals", goals);
+  }else{
+    res.render("goals_alternate", goals)
+  }
 };
