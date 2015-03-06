84cbe0f62387da71061e22070571de03863636db
Akshat Vasavada <akshatvas@gmail.com>
2015-03-05 18:37:23 -0800

    revision

--- routes/goals.js
+++ routes/goals.js
@@ -1,5 +1,11 @@
 var goals = require('../goals.json');
 
 exports.view = function(req, res){
-	res.render('goals', goals);
+	var random_num = Math.random();
+
+	if(random_num > 0.5){
+	  res.render("goals", projects);
+	}else{
+	  res.render("goals_alternate", projects)
+	}
 };
